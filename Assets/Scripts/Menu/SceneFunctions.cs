eea9a2ad8b01159f4d1d413adc01c4e0d6152ed0
Devin Sturgeon <dwsturge@gmail.com>
2025-05-10 14:44:20 -0400

    more ui updates

--- Assets/Scripts/Menu/SceneFunctions.cs
+++ Assets/Scripts/Menu/SceneFunctions.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SceneFunctions : MonoBehaviour
 {
     private GameObject settingsObj;
     private GameObject endScreenObj;
 
+    [SerializeField] Button Menu;
+    [SerializeField] Button Restart;
 
+    [SerializeField] string currentSceneName = "MainTesting";
+    [SerializeField] string titleSceneName = "Title";
+
+    public bool settingsUseable = true;
     void Awake()
     {
         settingsObj = GameObject.Find("SettingsPanel");
@@ -14,27 +22,49 @@ public class SceneFunctions : MonoBehaviour
         endScreenObj = GameObject.Find("EndrunScreen");
         endScreenObj.SetActive(false);
 
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    private void Start()
+    {
+        Restart.onClick.AddListener(delegate {
+            LoadLevel(currentSceneName);
+        });
+
+        Menu.onClick.AddListener(delegate {
+            LoadLevel(titleSceneName);
+        });
+
+
     }
 
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (settingsUseable)
         {
-            if (settingsObj.activeSelf)
+            if (Input.GetKeyDown(KeyCode.Escape))
             {
-                settingsObj.SetActive(false);
-                Time.timeScale = 1f;
-                PlayerController.instance.isPaused = false;
-            }
-            else
-            {
-                settingsObj.SetActive(true);
-                Time.timeScale = 0f;
-                PlayerController.instance.isPaused = true;
+                if (settingsObj.activeSelf)
+                {
+                    settingsObj.SetActive(false);
+                    Time.timeScale = 1f;
+                    PlayerController.instance.isPaused = false;
+                }
+                else
+                {
+                    settingsObj.SetActive(true);
+                    Time.timeScale = 0f;
+                    PlayerController.instance.isPaused = true;
+                }
             }
         }
+    }
+
+    void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
     }
 
 }
